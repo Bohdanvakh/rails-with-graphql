2c3e64479a7cfcbf13bfa81f3453b0de671fc71d
Bohdan Vakh <Bohdanvah@gmail.com>
2024-01-20 14:20:44 +0200

    feat/add password to create_user mutation

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,12 +1,13 @@
 class Mutations::CreateUser < Mutations::BaseMutation
   argument :name, String, required: true
   argument :email, String, required: true
+  argument :password, String, required: true
 
   field :user, Types::UserType, null: false
   field :errors, [String], null: false
 
-  def resolve(name:, email:)
-    user = User.new(name:, email:)
+  def resolve(name:, email:, password:)
+    user = User.new(name:, email:, password:)
 
     if user.save
       { user: user, errors: [] }
